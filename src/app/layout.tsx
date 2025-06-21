e299c183e70f629e0ae783c500dee603a49fa6b0
Afp123r <a19561897@gmail.com>
2025-06-21 22:55:45 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Tomcom - Portfolio',
+  title: 'Portfolio',
   description: 'Deep Learning Engineer & Data Scientist Portfolio',
 };
 
